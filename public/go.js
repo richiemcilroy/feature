3074959f65ac46c2ab85438efb5e30d4090b7798
Richie McIlroy <richie@mcilroy.co>
2021-08-04 22:56:06 +0100

    Logging change for go public script

--- public/go.js
+++ public/go.js
@@ -8,12 +8,12 @@ var projectso = async function() {
 
     fetch(rootAPIDomain+'projectDetails')
     .then(response => {
-      if (!response.ok) {
-        console.log(`Feature.so HTTP error! status: ${response.status}`)
-      }
-
-      console.log(response.body);
-    }).catch(e => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(e => {
       console.log('There has been a problem fetching your project from Feature.so - ' + e.message);
     });
   }
