0f4c9e2db9b7b63d98133c51e08a12242c7a78fe
Richie McIlroy <richie@mcilroy.co>
2021-08-04 21:37:09 +0100

    Get data

--- public/go.js
+++ public/go.js
@@ -1,4 +1,23 @@
-var projectso = function(e) {
+var projectso = function() {
   "use strict";
-  console.log("Feature.so Loaded");
+
+  const rootAPIDomain = 'https://feature.so/api/';
+
+  async function getData(){
+    fetch(rootAPIDomain+'projectDetails')
+    .then(response => {
+      if (!response.ok) {
+        console.log(`Feature.so HTTP error! status: ${response.status}`)
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(e => {
+      console.log('There has been a problem fetching your project from Feature.so - ' + e.message);
+    });
+  }
+
+  await getData();
+  
 }({});
